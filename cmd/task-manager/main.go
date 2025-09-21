f42b8ccb1a338618610f557e5940b89de1dee1a3
kinesisss <avonrodt@gmail.com>
2025-09-20 21:34:58 -0300

    feat: added persistance features

--- cmd/task-manager/main.go
+++ cmd/task-manager/main.go
@@ -3,34 +3,46 @@ import (
 	"github.com/kinesisss/kinesis-cli/internal/cliparser"
 	"github.com/kinesisss/kinesis-cli/internal/storagehandler"
 	"fmt"
+	"encoding/json"
 	"time"
 )
 
 type UserTask struct{
-	name string
-	createdAt string
+	Name string
+	CreatedAt string
 }
 
 func createTask(taskName string, creationTime time.Time) UserTask{
 	userTask := UserTask{
-		name: taskName,
-		createdAt: creationTime.String(),
+		Name: taskName,
+		CreatedAt: creationTime.String(),
 	}
 	return userTask
 }
 
 func (u UserTask) renderUserTask() string{
-	return fmt.Sprintf("<%v> task created at <%v> \n", u.name, u.createdAt)
+	return fmt.Sprintf("<%v> task created at <%v> \n", u.Name, u.CreatedAt)
 }
 
 func main(){
 	taskName := cliparser.TaskFlagParser()
 	timeNow := time.Now()
 	currentTask := createTask(taskName, timeNow)
+	storagePath := "./storage/tasks.json" 
 	fmt.Printf(currentTask.renderUserTask())
-	canStore, _ := storagehandler.CheckStorage(".", "storage")
-	if canStore{
-		fmt.Printf("we can definetely store your data\n")
+	canStore, _ := storagehandler.CheckStorage(storagePath)
+	jbytes, jerr := json.Marshal(currentTask)
+	if jerr != nil {
+		fmt.Printf("... something went transforming your task")
 	}
+	if !canStore{
+		fmt.Printf("... setting up your storage system")
+		_, err := storagehandler.CreateStorage(storagePath)
+		if err != nil {
+			fmt.Printf("something happened while setting up your storage")
+			fmt.Printf(err.Error())
+		}
+	} 
+	storagehandler.WriteToStorageFile(storagePath, jbytes, 0666)
 
 }
--- internal/storagehandler/storagehandler.go
+++ internal/storagehandler/storagehandler.go
@@ -2,19 +2,32 @@ package storagehandler
 
 import (
 	"os"
+	"fmt"
 )
 
-func CheckStorage(dirName string, nameToCheck string) (bool, error){
-	files, err := os.ReadDir(dirName)
+func CheckStorage(dirName string) (bool, error){
+	_, err := os.Stat(dirName)
 
 	if err != nil {
 		return false, err
 	}
+	return true, nil
+}
 
-	for _, file := range files{
-		if file.Name() == nameToCheck{
-			return true, nil
-		}
+func CreateStorage(storageDir string) (os.File, error){
+	rFilePtr, fileErr := os.Create(storageDir)
+	if fileErr != nil{
+		return *rFilePtr, fileErr
 	}
-	return false, nil
+	return *rFilePtr, nil
+}
+
+func WriteToStorageFile(storagePath string, data []byte, perm os.FileMode) {
+	fmt.Printf("... persisting your task")
+	werr := os.WriteFile(storagePath, data, perm)
+	if werr != nil{
+		fmt.Printf("something went wrong when persisting your file")
+		fmt.Printf(werr.Error())
+	}
+	fmt.Printf("file has been persisted succesfully")
 }
